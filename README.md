a7542abd9cd63e59598bc58516c2695bbac5003a
kchudy <krzysztof.chudy@clebre.com>
2020-09-20 11:09:28 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#Demo application for Infoshare Academy Webinar
+
+To start, type: ```mvn spring-boot:run```.
+
+Server is running on default settings on port 8080.
+
+Server does not require any database. It runs on embedded in-memory H2 database.
+
+Have fun :-) 
---
+++ src/main/java/com/isa/contacts/ContactsApplication.java
@@ -0,0 +1,21 @@
+package com.isa.contacts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableJpaRepositories
+@EnableTransactionManagement
+public class ContactsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactsApplication.class, args);
+    }
+
+}
